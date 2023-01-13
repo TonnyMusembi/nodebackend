a88fd32da81feb97b67114447b7c5aa112f5a78a
TonnyMusembi <musembutonnyj@gmail.com>
2023-01-13 11:42:13 +0300

    update

--- index.js
+++ index.js
@@ -1,32 +1,26 @@
-const express = require('express');
+const express = require("express");
+const jwt = require("jsonwebtoken");
 // const app = require(express);
 // app.use(express.json());
 const app = express();
 
 const port = 3005;
-
+app.get("/token", (res, req) => {
+    const payload = {
+        name: "Tony"
+    };
+    const token = jwt.sign(payload, "super secret key");
+    res.send(token);
+});
 // app.get("/", (res, req) => {
 //     res.send("Hello")
 
 // });
-app.get("/", (req, res) => {
+app.get("/customer", (req, res) => {
     res.send("Hello, World!");
 });
 
-
 app.post("/login", async(res, req) => {
-
-    try {
-
-
-
-
-    } catch (error) {
-
-    }
-
-
-
-
+    try {} catch (error) {}
 });
 app.listen(port, () => console.log(`app running on http://127.0.0.1:${port}`));
