72ae43473acb6f6c792cb58cccaad251fbf04a87
TonnyMusembi <musembutonnyj@gmail.com>
2023-02-03 17:17:51 +0300

    add

---
+++ data.js
@@ -0,0 +1,15 @@
+const products = [
+    { id: 1, name: "iPhone", price: 800 },
+    { id: 2, name: "iPad", price: 650 },
+    { id: 3, name: "iWatch", price: 750 },
+];
+
+module.exports = products;
+
+const sales = [
+    { id: 1, name: "test", price: 400 },
+    { id: 2, name: "login", price: 200 },
+    { id: 3, name: "pet", price: 500 },
+];
+
+module.exports = sales;
